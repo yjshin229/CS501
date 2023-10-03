d26f7a0e61d830a8e7654387c452fa7825c86f58
Thanh <thanhthuynh7@gmail.com>
2023-10-03 16:53:17 -0400

    added 2 unit tests

--- hw03/GeoQuizApp/app/src/test/java/com/example/geoquizapp/QuizViewModelTest.kt
+++ hw03/GeoQuizApp/app/src/test/java/com/example/geoquizapp/QuizViewModelTest.kt
@@ -2,9 +2,13 @@ package com.example.geoquizapp
 
 import androidx.lifecycle.SavedStateHandle
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
-class QuizViewModelTest { @Test
+class QuizViewModelTest {
+
+    @Test
     fun providesExpectedQuestionText() {
         val savedStateHandle = SavedStateHandle()
         val quizViewModel = QuizViewModel(savedStateHandle)
@@ -18,5 +22,26 @@ class QuizViewModelTest { @Test
         assertEquals(R.string.question_asia, quizViewModel.currentQuestionText)
         quizViewModel.moveToNext()
         assertEquals(R.string.question_australia, quizViewModel.currentQuestionText)
+    }
+
+    @Test
+    fun correctlyHandlesCheatingState() {
+        val savedStateHandle = SavedStateHandle()
+        val quizViewModel = QuizViewModel(savedStateHandle)
+
+        // Assume cheating on the first question
+        quizViewModel.questionsCheated[0] = true
+
+        assertTrue(quizViewModel.questionsCheated[0])  // Assert we cheated on the first question
+
+        // Move to the next question
+        quizViewModel.moveToNext()
+
+        assertFalse(quizViewModel.questionsCheated[quizViewModel.currentIndex])  // Assert we haven't cheated on the second question
+
+        // Cheat on the second question and verify
+        quizViewModel.questionsCheated[quizViewModel.currentIndex] = true
+
+        assertTrue(quizViewModel.questionsCheated[quizViewModel.currentIndex])  // Assert we cheated on the second question
     }
 }
