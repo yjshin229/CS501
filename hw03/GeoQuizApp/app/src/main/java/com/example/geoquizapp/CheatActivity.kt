7a299d325f8efdcbd67addd15203047c6e3cb9b6
Thanh <thanhthuynh7@gmail.com>
2023-10-03 16:35:17 -0400

    fix cheating rotation loophole

--- hw03/GeoQuizApp/app/src/main/java/com/example/geoquizapp/CheatActivity.kt
+++ hw03/GeoQuizApp/app/src/main/java/com/example/geoquizapp/CheatActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.geoquizapp.databinding.ActivityCheatBinding
+import androidx.activity.viewModels
 
 const val EXTRA_ANSWER_SHOWN = "com.example.geoquizapp.answer_shown"
 private const val EXTRA_ANSWER_IS_TRUE =
@@ -13,26 +14,35 @@ private const val EXTRA_ANSWER_IS_TRUE =
 class CheatActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCheatBinding
+    private val cheatViewModel: CheatViewModel by viewModels()
 
     private var answerIsTrue = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityCheatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        cheatViewModel.isAnswerShown.takeIf { it }?.let {
+            showAnswer()
+        }
 
         answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
 
         binding.showAnswerButton.setOnClickListener {
-            val answerText = when {
-                answerIsTrue -> R.string.true_button
-                else -> R.string.false_button
-            }
-            binding.answerTextView.setText(answerText)
+            showAnswer()
             setAnswerShownResult(true)
         }
 
+    }
+
+    private fun showAnswer() {
+        val answerText = when {
+            answerIsTrue -> R.string.true_button
+            else -> R.string.false_button
+        }
+        binding.answerTextView.setText(answerText)
+        cheatViewModel.isAnswerShown = true
     }
 
     private fun setAnswerShownResult(isAnswerShown: Boolean) {
