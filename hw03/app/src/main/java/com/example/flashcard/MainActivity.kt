cd309f9a2263930eeb8f84368d1d35276e29ba40
Youngjin <yjshin229@gmail.com>
2023-10-03 03:41:53 -0400

    implement logic

--- hw03/app/src/main/java/com/example/flashcard/MainActivity.kt
+++ hw03/app/src/main/java/com/example/flashcard/MainActivity.kt
@@ -2,19 +2,21 @@ package com.example.flashcard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.widget.Toast
 import com.example.flashcard.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private var answer_keys = IntArray(10)
-    var first_numbers = IntArray(10)
-    var second_numbers = IntArray(10)
-    var submitted_answers = IntArray(10)
-    var question_index = 0
-    var is_addition = BooleanArray(10)
+    private var answerKeys = IntArray(10)
+    private var firstNumbers = IntArray(10)
+    private var secondNumbers = IntArray(10)
+    private var submittedAnswers = IntArray(10)
+    private var questionIndex = 0
+    private var isAddition = BooleanArray(10)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +26,59 @@ class MainActivity : AppCompatActivity() {
         binding.generateButton.setOnClickListener{
             generateQuestions()
             binding.generateButton.isEnabled = false
+            binding.submitButton.isEnabled = true
+            displayQuestion()
+        }
+        binding.submitButton.setOnClickListener{
+            if(binding.submitEdittext.text.isEmpty()){
+                Toast.makeText(
+                    this,
+                    R.string.empty_input,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }else{
+                submittedAnswers[questionIndex] = binding.submitEdittext.text.toString().toInt()
+                if(questionIndex !=9){
+                    binding.submitEdittext.text.clear()
+                    questionIndex ++
+                    displayQuestion()
+                }else{
+                    questionIndex = 0
+                    binding.generateButton.isEnabled = true
+                    binding.submitButton.isEnabled = false
+                    resetQuestion()
+                    getResult()
+                }
+            }
+
+
+        }
+        binding.submitButton.setOnKeyListener{v, keyCode, event ->
+            if(keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN){
+                binding.submitButton.performClick()
+                true
+            }else{
+                false
+            }
         }
 
+    }
+    private fun displayQuestion(){
+        binding.firstNumber.text = firstNumbers[questionIndex].toString()
+        binding.secondNumber.text = secondNumbers[questionIndex].toString()
+        binding.operator.text = if (isAddition[questionIndex] ) "+" else "-"
+        if(isAddition[questionIndex]) binding.additionRadio.isChecked = true else binding.subtractionRadio.isChecked = true
+        binding.questionIndexView.text =(questionIndex + 1).toString()
+    }
+
+    private fun resetQuestion(){
+        binding.firstNumber.text = ""
+        binding.secondNumber.text = ""
+        binding.operator.text = ""
+        binding.submitEdittext.text.clear()
+        binding.additionRadio.isChecked = false
+        binding.subtractionRadio.isChecked = false
+        binding.questionIndexView.text =(questionIndex + 1).toString()
     }
 
     private fun getTwoRandom() : IntArray{
@@ -41,7 +94,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateAnswer(arr: IntArray, index: Int):Int{
-        if(is_addition[index]){
+        if(isAddition[index]){
             return arr[0] + arr[1]
         }else{
             return arr[0] - arr[1]
@@ -51,13 +104,32 @@ class MainActivity : AppCompatActivity() {
     private fun generateQuestions(){
         for (i in 0..9){
             val operator = getOperator()
-            is_addition[i] = operator
+            isAddition[i] = operator
             val ints = getTwoRandom()
             val answer = calculateAnswer(ints, i)
-            first_numbers[i] = ints[0]
-            second_numbers[i] = ints[1]
-            answer_keys[i] = answer
+            firstNumbers[i] = ints[0]
+            secondNumbers[i] = ints[1]
+            answerKeys[i] = answer
         }
     }
+
+    private fun getResult(){
+        var score = 0
+
+        for(i in 0..9){
+            if(answerKeys[i] == submittedAnswers[i]){
+                score++
+            }
+        }
+
+        val formattedText = "$score out of 10"
+        Toast.makeText(
+            this,
+            formattedText,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+
 
 }
