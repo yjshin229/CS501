578a789da94dfc1e4c9cee2729f964ef94fe0cce
Thanh <thanhthuynh7@gmail.com>
2023-10-17 18:19:55 -0400

    added images and shake functionality for each direction's image

--- DirectionFlingApp/app/src/main/java/com/example/directionflingapp/EastActivity.kt
+++ DirectionFlingApp/app/src/main/java/com/example/directionflingapp/EastActivity.kt
@@ -5,15 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.GestureDetector
 import android.view.MotionEvent
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.Toast
 
 class EastActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
     private lateinit var gestureDetector: GestureDetector
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_north)
+        setContentView(R.layout.activity_east)
 
         gestureDetector = GestureDetector(this, this)
+
+        // Displaying the string resource for North direction
+        Toast.makeText(this, getString(R.string.direction_east), Toast.LENGTH_SHORT).show()
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -24,13 +31,14 @@ class EastActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
     }
 
     override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
-        if (e2.y > e1.y) {
-            // Downward fling detected in NorthActivity, returning to MainActivity
+        if (velocityX < 0) {
+            // Leftward fling detected in EestActivity, returning to MainActivity
             startActivity(Intent(this, MainActivity::class.java))
             return true
         }
         return false
     }
+
 
     override fun onDown(p0: MotionEvent): Boolean {
         return false
@@ -56,5 +64,12 @@ class EastActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
     override fun onSingleTapUp(p0: MotionEvent): Boolean {
         return false
     }
+
+    fun onShake() {
+        val shake: Animation = AnimationUtils.loadAnimation(this, R.anim.shake_animation)
+        val imgView: ImageView = findViewById(R.id.east_cat_image) // change the ID based on the activity
+        imgView.startAnimation(shake)
+    }
+
 
 }
--- DirectionFlingApp/app/src/main/java/com/example/directionflingapp/WestActivity.kt
+++ DirectionFlingApp/app/src/main/java/com/example/directionflingapp/WestActivity.kt
@@ -5,15 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.GestureDetector
 import android.view.MotionEvent
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.Toast
 
 class WestActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
     private lateinit var gestureDetector: GestureDetector
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_north)
+        setContentView(R.layout.activity_west)
 
         gestureDetector = GestureDetector(this, this)
+
+        // Displaying the string resource for North direction
+        Toast.makeText(this, getString(R.string.direction_west), Toast.LENGTH_SHORT).show()
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -24,8 +31,8 @@ class WestActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
     }
 
     override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
-        if (e2.y > e1.y) {
-            // Downward fling detected in NorthActivity, returning to MainActivity
+        if (velocityX > 0) {
+            // Rightward fling detected in WastActivity, returning to MainActivity
             startActivity(Intent(this, MainActivity::class.java))
             return true
         }
@@ -56,5 +63,12 @@ class WestActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
     override fun onSingleTapUp(p0: MotionEvent): Boolean {
         return false
     }
+
+    fun onShake() {
+        val shake: Animation = AnimationUtils.loadAnimation(this, R.anim.shake_animation)
+        val imgView: ImageView = findViewById(R.id.south_cat_image) // change the ID based on the activity
+        imgView.startAnimation(shake)
+    }
+
 
 }
