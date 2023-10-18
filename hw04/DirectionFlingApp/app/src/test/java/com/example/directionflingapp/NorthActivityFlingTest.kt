e56fbd8b50a63ed191d35f47b0a3777cff4a2223
Thanh <thanhthuynh7@gmail.com>
2023-10-17 20:25:18 -0400

    added unit and instrument tests

--- hw04/DirectionFlingApp/app/src/test/java/com/example/directionflingapp/NorthActivityFlingTest.kt
+++ hw04/DirectionFlingApp/app/src/test/java/com/example/directionflingapp/NorthActivityFlingTest.kt
@@ -1,16 +1,43 @@
 package com.example.directionflingapp
 
-import android.view.MotionEvent
-import org.junit.Assert.*
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.android.controller.ActivityController
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import android.content.Intent
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.swipeDown
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.IsEqual.equalTo
+import org.robolectric.Shadows.shadowOf
 
+@RunWith(RobolectricTestRunner::class)
 class NorthActivityFlingTest {
 
+    private lateinit var activityController: ActivityController<NorthActivity>
+    private lateinit var activity: NorthActivity
+
+    @Before
+    fun setup() {
+        activityController = Robolectric.buildActivity(NorthActivity::class.java).create().visible()
+        activity = activityController.get()
+    }
+
+    @After
+    fun tearDown() {
+        activityController.pause().stop().destroy()
+    }
+
     @Test
-    fun testOnFlingSouthDirection() {
-        val activity = NorthActivity()
-        val e1 = MotionEvent.obtain(0, 0, MotionEvent.ACTION_DOWN, 100f, 100f, 0)
-        val e2 = MotionEvent.obtain(0, 0, MotionEvent.ACTION_UP, 100f, 200f, 0)
-        assertTrue(activity.onFling(e1, e2, 0f, 100f))
+    fun testFlingDownwardReturnsToMain() {
+        onView(withId(R.id.north_cat_image)).perform(swipeDown())
+
+        val expectedIntent = Intent(activity, MainActivity::class.java)
+        val actualIntent = shadowOf(activity).nextStartedActivity
+        assertThat(actualIntent.component, equalTo(expectedIntent.component))
     }
 }
