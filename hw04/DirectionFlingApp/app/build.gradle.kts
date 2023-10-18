2760d2c2d9ecd5c34d5b7f5065e7ab7998ddf417
Thanh <thanhthuynh7@gmail.com>
2023-10-17 20:25:18 -0400

    added unit and instrument tests

--- hw04/DirectionFlingApp/app/build.gradle.kts
+++ hw04/DirectionFlingApp/app/build.gradle.kts
@@ -41,7 +41,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.test.ext:junit-ktx:1.1.5")
+    implementation("androidx.test.espresso:espresso-intents:3.5.1")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.robolectric:robolectric:4.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- hw04/DirectionFlingApp/app/src/test/java/com/example/directionflingapp/SouthActivityFlingTest.kt
+++ hw04/DirectionFlingApp/app/src/test/java/com/example/directionflingapp/SouthActivityFlingTest.kt
@@ -1,16 +1,43 @@
 package com.example.directionflingapp
 
-import android.view.MotionEvent
-import org.junit.Assert.*
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.android.controller.ActivityController
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import android.content.Intent
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.swipeUp
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.IsEqual.equalTo
+import org.robolectric.Shadows.shadowOf
 
+@RunWith(RobolectricTestRunner::class)
 class SouthActivityFlingTest {
 
+    private lateinit var activityController: ActivityController<SouthActivity>
+    private lateinit var activity: SouthActivity
+
+    @Before
+    fun setup() {
+        activityController = Robolectric.buildActivity(SouthActivity::class.java).create().visible()
+        activity = activityController.get()
+    }
+
+    @After
+    fun tearDown() {
+        activityController.pause().stop().destroy()
+    }
+
     @Test
-    fun testOnFlingNorthDirection() {
-        val activity = SouthActivity()
-        val e1 = MotionEvent.obtain(0, 0, MotionEvent.ACTION_DOWN, 100f, 200f, 0)
-        val e2 = MotionEvent.obtain(0, 0, MotionEvent.ACTION_UP, 100f, 100f, 0)
-        assertTrue(activity.onFling(e1, e2, 0f, -100f))
+    fun testFlingUpwardReturnsToMain() {
+        onView(withId(R.id.south_cat_image)).perform(swipeUp())
+
+        val expectedIntent = Intent(activity, MainActivity::class.java)
+        val actualIntent = shadowOf(activity).nextStartedActivity
+        assertThat(actualIntent.component, equalTo(expectedIntent.component))
     }
 }
